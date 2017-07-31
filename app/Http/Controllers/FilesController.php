6a31a1d135941b802008cab7c45872e7d0511bc6
Korko <jeremy.lemesle@korko.fr>
2017-07-31 21:10:28 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -2,21 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class FilesController extends Controller
 {
-    public function list() {
-
-	$files = [];
+    public function list()
+    {
+        $files = [];
         $breadcrumbs = [];
 
         return view('listing', [
-            'dir' => '/',
-            'files' => $files,
+            'dir'         => '/',
+            'files'       => $files,
             'breadcrumbs' => $breadcrumbs,
-            'canZip' => false
+            'canZip'      => false,
         ]);
-
     }
 }
